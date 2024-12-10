a0369c3e77576ca57278778cc673ef0d540859d6
Matthew Fitch <matthew.fitch26@d155.org>
2024-12-10 05:40:05 -0800

    STUFF!?

--- UsefulAppStark/CreatorViewController.swift
+++ UsefulAppStark/CreatorViewController.swift
@@ -61,6 +61,10 @@ class CreatorViewController: UIViewController, UITextViewDelegate {
              nuhUH(alertText: "There is no definition yet")
          } else {
              Info.flashCardArray.append(Flashcard(word: wordTextView.text, def: defTextView.text, stared: false))
+             if let something = try? Info.encoder.encode(Info.flashcardSets)
+             {
+                 Info.defaults.set(something, forKey: "allSets")
+             }
           }
          instantGratification()
      }
