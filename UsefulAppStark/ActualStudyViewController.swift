a68f85a0982432a416679172aaf27f82b4e6436e
MonsterusCow <ryan.stark26@d155.org>
2024-12-11 07:46:14 -0600

    star in study

--- UsefulAppStark/ActualStudyViewController.swift
+++ UsefulAppStark/ActualStudyViewController.swift
@@ -14,6 +14,7 @@ class ActualStudyViewController: UIViewController {
     @IBOutlet weak var noteCardImage: UIImageView!
     @IBOutlet weak var wordPart: UILabel!
     @IBOutlet weak var starImage: UIImageView!
+    @IBOutlet weak var starSwitch: UISwitch!
     var number = 0
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -37,37 +38,148 @@ class ActualStudyViewController: UIViewController {
         }
     }
     
+    @IBAction func starSwitch(_ sender: Any) {
+        if !starSwitch.isOn {
+            if howManyStared() > 0 {
+                if !Info.flashCardArray[number].stared {
+                    var repeet = false
+                    repeat {
+                        repeet = false
+                        if number == Info.flashCardArray.count-1 {
+                            number = 0
+                        } else {
+                            number += 1
+                        }
+                        if Info.flashCardArray[number].stared {
+                            noteCardImage.image = UIImage(named: "notecard")
+                            wordPart.text = "\(Info.flashCardArray[number].word)"
+                            if Info.flashCardArray[number].stared{
+                                starImage.image = UIImage(systemName: "star.fill")
+                            } else {
+                                starImage.image = UIImage(systemName: "star")
+                            }
+                        } else {
+                            repeet = true
+                        }
+                    } while (repeet)
+                }
+            } else {
+                starSwitch.isOn = true
+                error(alertMessage: "There are no starred flashcards", alertTitle: "No Stars")
+            }
+        }
+    }
+    
     @IBAction func finishButton(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
     @IBAction func backButton(_ sender: Any) {
-        if number == 0 {
-            number = Info.flashCardArray.count-1
+        if !starSwitch.isOn {
+            if howManyStared() > 0{
+                var repeet = false
+                repeat {
+                    repeet = false
+                    if number == 0 {
+                        number = Info.flashCardArray.count-1
+                    } else {
+                        number -= 1
+                    }
+                    if Info.flashCardArray[number].stared {
+                        noteCardImage.image = UIImage(named: "notecard")
+                        wordPart.text = "\(Info.flashCardArray[number].word)"
+                        if Info.flashCardArray[number].stared{
+                            starImage.image = UIImage(systemName: "star.fill")
+                        } else {
+                            starImage.image = UIImage(systemName: "star")
+                        }
+                    } else {
+                        repeet = true
+                    }
+                } while (repeet)
+            } else {
+                starSwitch.isOn = true
+                if number == 0 {
+                    number = Info.flashCardArray.count-1
+                } else {
+                    number -= 1
+                }
+                    noteCardImage.image = UIImage(named: "notecard")
+                    wordPart.text = "\(Info.flashCardArray[number].word)"
+                    if Info.flashCardArray[number].stared{
+                        starImage.image = UIImage(systemName: "star.fill")
+                    } else {
+                        starImage.image = UIImage(systemName: "star")
+                    }
+            }
         } else {
-            number -= 1
-        }
-        noteCardImage.image = UIImage(named: "notecard")
-        wordPart.text = "\(Info.flashCardArray[number].word)"
-        if Info.flashCardArray[number].stared{
-            starImage.image = UIImage(systemName: "star.fill")
-        } else {
-        starImage.image = UIImage(systemName: "star")
+            if number == 0 {
+                number = Info.flashCardArray.count-1
+            } else {
+                number -= 1
+            }
+                noteCardImage.image = UIImage(named: "notecard")
+                wordPart.text = "\(Info.flashCardArray[number].word)"
+                if Info.flashCardArray[number].stared{
+                    starImage.image = UIImage(systemName: "star.fill")
+                } else {
+                    starImage.image = UIImage(systemName: "star")
+                }
         }
     }
     @IBAction func forwardButton(_ sender: Any) {
-        if number == Info.flashCardArray.count-1 {
-            number = 0
+        if !starSwitch.isOn {
+            if howManyStared() > 0{
+                var repeet = false
+                repeat {
+                    repeet = false
+                    if number == Info.flashCardArray.count-1 {
+                        number = 0
+                    } else {
+                        number += 1
+                    }
+                    if Info.flashCardArray[number].stared {
+                        noteCardImage.image = UIImage(named: "notecard")
+                        wordPart.text = "\(Info.flashCardArray[number].word)"
+                        if Info.flashCardArray[number].stared{
+                            starImage.image = UIImage(systemName: "star.fill")
+                        } else {
+                            starImage.image = UIImage(systemName: "star")
+                        }
+                    } else {
+                        repeet = true
+                    }
+                } while (repeet)
+            } else {
+                starSwitch.isOn = true
+                if number == Info.flashCardArray.count-1 {
+                    number = 0
+                } else {
+                    number += 1
+                }
+                    noteCardImage.image = UIImage(named: "notecard")
+                    wordPart.text = "\(Info.flashCardArray[number].word)"
+                    if Info.flashCardArray[number].stared{
+                        starImage.image = UIImage(systemName: "star.fill")
+                    } else {
+                        starImage.image = UIImage(systemName: "star")
+                    }
+            }
         } else {
-            number += 1
-        }
-        noteCardImage.image = UIImage(named: "notecard")
-        wordPart.text = "\(Info.flashCardArray[number].word)"
-        if Info.flashCardArray[number].stared{
-            starImage.image = UIImage(systemName: "star.fill")
-        } else {
-        starImage.image = UIImage(systemName: "star")
+            if number == Info.flashCardArray.count-1 {
+                number = 0
+            } else {
+                number += 1
+            }
+                noteCardImage.image = UIImage(named: "notecard")
+                wordPart.text = "\(Info.flashCardArray[number].word)"
+                if Info.flashCardArray[number].stared{
+                    starImage.image = UIImage(systemName: "star.fill")
+                } else {
+                    starImage.image = UIImage(systemName: "star")
+                }
         }
     }
+    
     @IBAction func flipButton(_ sender: Any) {
         if noteCardImage.image == UIImage(named: "notecard"){
             noteCardImage.image = UIImage(named: "blank")
@@ -93,11 +205,30 @@ class ActualStudyViewController: UIViewController {
         }
     }
     
+    func howManyStared() -> Int{
+        var temp = [Int]()
+        for i in 0..<Info.flashCardArray.count {
+            if Info.flashCardArray[i].stared{
+                temp.append(i)
+            }
+        }
+        return temp.count
+    }
+    
     func notEnoughCardsError(alertMessage: String, alertTitle: String)
     {
         let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: UIAlertController.Style.alert)
         let okAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default){ (action) in
             self.tabBarController?.selectedIndex = Info.prevTabBar
+        }
+        alert.addAction(okAction)
+        self.present(alert, animated: true)
+    }
+    
+    func error(alertMessage: String, alertTitle: String)
+    {
+        let alert = UIAlertController(title: alertTitle, message: alertMessage, preferredStyle: UIAlertController.Style.alert)
+        let okAction = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default){ (action) in
         }
         alert.addAction(okAction)
         self.present(alert, animated: true)
