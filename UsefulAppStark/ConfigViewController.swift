b8964ed667c0f35255bad561c96eabce6ca45656
MonsterusCow <ryan.stark26@d155.org>
2024-12-17 07:48:55 -0600

    thin

--- UsefulAppStark/ConfigViewController.swift
+++ UsefulAppStark/ConfigViewController.swift
@@ -53,6 +53,7 @@ class ConfigViewController: UIViewController {
                 Settings.quizType = "normal"
                 staredQuizLabel.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
                 staredBackground.tintColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+                staredQuizLabel.textColor = UIColor(red: 1, green: 1, blue: 1, alpha: 1)
             }
         }
     }
